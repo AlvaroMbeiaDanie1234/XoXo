097002df3409d6d7300ee9327515b6223cd896e4
AlvaroMbeiaDaniel1234 <alvarombeiadanielmiguel@gmail.com>
2026-07-21 14:18:28 +0100

    fix: use server ip for supabase proxy

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/supabase-api/:path*',
+        destination: 'http://204.168.143.175:8010/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
